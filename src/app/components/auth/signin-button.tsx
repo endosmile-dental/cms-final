e59598fdabc49a79f97f7ff5123372fe6e9a81e9
MJ150296 <joshi.mayank5286@gmail.com>
2025-06-25 18:37:01 +0530

    updated signin page

--- src/app/components/auth/signin-button.tsx
+++ src/app/components/auth/signin-button.tsx
@@ -10,7 +10,7 @@ export function SignIn() {
       className="w-full h-screen flex flex-col items-center justify-center text-white"
     >
       <h1 className="text-7xl font-bold tracking-wide">CMS</h1>
-      <h2 className="text-xl mb-10">A complete clinic solution</h2>
+      <h2 className="text-xl font-semibold mb-10">A complete clinic solution</h2>
       <button
         type="submit"
         className="p-2 rounded-lg bg-white hover:bg-black/50 hover:text-white text-black transition-colors duration-300"
